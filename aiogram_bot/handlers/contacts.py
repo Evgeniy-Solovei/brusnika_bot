f4b694753e072e9da6a07a6364440842bdfcd902
solovey.e.v <e.v.solovey@inbox.ru>
2025-01-28 00:02:59 +0300

    add project

--- aiogram_bot/handlers/contacts.py
+++ aiogram_bot/handlers/contacts.py
@@ -7,11 +7,10 @@ router = Router()
 @router.message(lambda message: message.text == "Контакты")
 async def handle_contacts(message: types.Message):
     contacts_text = (
-        "📞 <b>Телефон:</b> +7 (XXX) XXX-XX-XX\n"
-        "📧 <b>Почта:</b> rent@example.com\n"
-        "📱 <b>Telegram:</b> @rent_company\n"
-        "🌐 <b>ВКонтакте:</b> <a href='https://vk.com/rent_company'>vk.com/rent_company</a>\n"
-        "📸 <b>Instagram:</b> <a href='https://instagram.com/rent_company'>instagram.com/rent_company</a>"
+        "📞 <b>Телефон:</b> +7 936 000 6257\n"
+        "📧 <b>Почта:</b> ooo.brusnika@yandex.ru\n"
+        "📱 <b>Telegram:</b> +7 936 000 6257\n"
+        "💬 <b>WhatsApp:</b> +7 936 000 6257\n"
     )
     back_button = types.KeyboardButton(text="Назад")
     keyboard = types.ReplyKeyboardMarkup(keyboard=[[back_button]], resize_keyboard=True)
